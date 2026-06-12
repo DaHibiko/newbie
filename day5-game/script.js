0dd4f078affa93815bfe83e420d735809fb09751
Hibiki <kohiicup@gmail.com>
2026-06-12 18:13:35 +0900

    配列やifを使った下地の再構築

--- day5-game/script.js
+++ day5-game/script.js
@@ -1,66 +1,19 @@
-let gold = 0;
-let hasSword = false;
+let gold = 1000
+let items = [
+    { name: "剣", price: 500 },
+    { name: "盾", price: 300 },
+    { name: "薬草", price: 50 },
+]
+function buyItem(index) {
 
-        function earnGold() {
-            gold = gold + 100;
-            document.getElementById("gold").textContent =
-            gold + "G";
-        }
+    if (gold >= items[index].price) {
 
-        function buySword() {
-            if (hasSword == true){
-            alert ("もう既に持っています");
-            }
-            else if (gold >= 500){
-                gold = gold - 500;
-                document.getElementById("gold").textContent = 
-                gold + "G";
-                hasSword = true;
+        gold = gold - items[index].price;
 
-            alert ("剣が買えました!");
-            } else {
-            alert ("お金が足りません");
-            }
-        } 
-/* 同じ関数は複数作れない、作った場合あとの関数が優先される */ 
-/* getElementById() はHTMLのidを探す。
-goldだと変数を探していることになるので、"gold" と書いて id="gold" を探す */
-/* gold → 変数 
-"gold" → HTMLのid */
-/* if
-↓
-最初の質問
+        document.getElementById("gold").textContent =
+        "所持金 " + gold + "G";
 
-else if
-↓
-最初がダメだったら次の質問
-
-else
-↓
-全部ダメだったら最後の処理 */
-
-        let items = [
-            "剣",
-            "盾",
-            "薬草"
-        ];
-        let prices = [
-            500,
-            300,
-            50
-        ];
-        function buyItem(index){
-            if (gold>= prices[index]){
-            
-            gold = gold - prices[index];
-            document.getElementById("gold").textContent =
-            gold + "G";
-
-            alert(items[index] + "を買いました");
-            
-        } else {
-            alert("お金が足りませんでした")
-        }
+        document.getElementById("message").textContent =
+        items[index].name + "を購入しました";
     }
-        console.log("エラー確認用") 
-        /*今やってる範囲だとhasSwordが腐って使えない*/
+}
