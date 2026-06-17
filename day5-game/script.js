25428811543728c85ee21b0caefe872600d882f5
Hibiki <kohiicup@gmail.com>
2026-06-17 19:05:30 +0900

    商品メニュー一覧と購入ボタンの併設と在庫の処理

--- day5-game/script.js
+++ day5-game/script.js
@@ -1,27 +1,49 @@
 let gold = 1000
 let items = [
-    { name: "剣", price: 500 },
-    { name: "盾", price: 300 },
-    { name: "薬草", price: 50 },
+    { name: "剣", price: 500, stock:1 },
+    { name: "盾", price: 300, stock:3 },
+    { name: "薬草", price: 50,},
 ]
-function buyItem(index) {
 
-    if (gold >= items[index].price) {
+function showItems(){
+let html = "";
 
+    for (let i = 0; i < items.length; i++) {
+        html += items[i].name + " ";
+        html += items[i].price + "G ";
+
+        if (items[i].stock <= 0) {
+            html += "売り切れ";
+        }
+        else {
+            html += "在庫: " + items[i].stock + " ";
+            html += '<button onclick="buyItem(' + i + ')">購入</button>';
+        }
+
+        html += "<br>";
+    }
+
+    document.getElementById("shop").innerHTML = html;
+}
+
+function buyItem(index){
+    if (items[index].stock <= 0) {
+    document.getElementById("message").textContent = 
+        "在庫がありません"
+    }
+    else if (gold < items[index].price) {
+        document.getElementById("message").textContent =
+        "お金が足りません"
+    }
+    else {
         gold = gold - items[index].price;
-
-        document.getElementById("gold").textContent =
-        "所持金 " + gold + "G";
+        items[index].stock = items[index].stock - 1;
 
         document.getElementById("message").textContent =
         items[index].name + "を購入しました";
+
+    showItems();
     }
 }
 
-let html = "";
-for (let i = 0; i < items.length; i++){
-    html += items[i].name + " " + items[i].price + "G" + "<br>";
-}
-
-document.getElementById("shop").innerHTML = html;
-/* html += `${items[i].name} ${items[i].price}G<br>`; 実務はこっちらしい、今の学習内容では↑で精一杯。 */
+showItems();
